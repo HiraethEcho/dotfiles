39ffef819cb99a0e0f3573301855a196290a22ec
arch24 <wyz2016zxc@outlook.com>
2025-01-16 22:49:22 +0800

    main use mate-polkit

--- dwm/.config/dwm/autostart.sh
+++ dwm/.config/dwm/autostart.sh
@@ -27,7 +27,8 @@ dunst &
 # if [ -z $(pidof blueberry-tray) ]; then
 # fi
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 # /usr/lib/xfce-polkit/xfce-polkit &
 # lxsession &
 
