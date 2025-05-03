326cb3e314448964ceacc9a7fc5d9196db6b8893
arch14 <wyz2016zxc@outlook.com>
2025-05-03 20:18:50 +0800

    update wm config

--- wm/.config/dwm/autostart.sh
+++ wm/.config/dwm/autostart.sh
@@ -1,35 +1,28 @@
 #!/bin/sh
-# pulseaudio --start
 
-killall xbindkeys
-mykeys &
-# if [ -z $(pidof xbindkeys)]; then
-# fi
+set +e
 
-killall fcitx5
-fcitx5 &
-# if [ -z $(pidof fcitx5)]; then
-# fi
-
-killall dunst
+if [ -z $(pidof fcitx5) ]; then
+fcitx5 --replace -d &
+fi
+if [ -z $(pidof dunst) ]; then
 dunst &
-# if [ -z $(pidof dunst)]; then
-# fi
+fi
+if [ -z $(pidof dwmblocks) ]; then
+dwmblocks &
+fi
+if [ -z $(pidof xbindkeys) ]; then
+mykeys &
+fi
 
-# killall picom
+# if [ -z $(pidof picom) ]; then
 # picom -b
-# if [ -z $(pidof picom)]; then
 # fi
-
-# killall blueman-applet
-# blueman-applet &
-# killall blueberry-tray
-# blueberry-tray &
 # if [ -z $(pidof blueberry-tray) ]; then
 # fi
 
-# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 # /usr/lib/xfce-polkit/xfce-polkit &
 # lxsession &
 
@@ -40,18 +33,9 @@ dunst &
 
 # killall battery_warning 
 # battery_warning &
-
-killall dwmblocks
-dwmblocks &
-
-# xnb &
-
-# if [ -z "$GNOME_KEYRING_CONTROL" ]; then
-#     eval $(gnome-keyring-daemon --start)
-# fi
-
-# if [ -z $(pidof dwmblocks) ]; then
-# fi
 #
+# killall dwmblocks
+# dwmblocks &
+
 # autossh -M 20000 -fNR 7004:localhost:2222 wyz@2408:8207:1963:c5d0:336f:8d38:d21a:7600
 autossh -M 20000 -fNR 7004:localhost:2222 wyz@$DEBIAN_IP
---
+++ wm/.config/maomao/autostart.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# pulseaudio --start
+set +e
+
+# obs
+# dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+# /usr/lib/xdg-desktop-portal-wlr &
+# night light
+# wlsunset -T 3501 -t 3500 &
+
+# wallpaper
+# swaybg -i ~/.config/maomao/wallpaper/room.png &
+
+# top bar
+# waybar -c ~/.config/maomao/waybar/config -s ~/.config/maomao/waybar/style.css &
+
+# dock
+# lavalauncher -c ~/.config/maomao/lavalauncher/lavalauncher.conf &
+
+# xwayland dpi scale
+# echo "Xft.dpi: 140" | xrdb -merge #dpi缩放
+# xrdb merge ~/.Xresources
+
+# ime input
+fcitx5 --replace -d &
+
+# keep clipboard content
+# wl-clip-persist --clipboard regular --reconnect-tries 0 &
+# clipboard content manager
+# wl-paste --type text --watch cliphist store & 
+# for private use not for you
+# cp ~/.config/eww/System-Menu/eww.yuck.hyprland  ~/.config/eww/System-Menu/eww.yuck
+# eww daemon &
+
+# idle to lightdown and shutdown screen
+# ~/.config/maomao/scripts/idle.sh &
+
+# inhibit by audio
+# sway-audio-idle-inhibit &
+
+# change light value and volume value by swayosd-client in keybind
+# swayosd-server &
+
+if [ -z $(pidof dunst)]; then
+dunst &
+fi
+
+
+
+# Permission authentication
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+
+# blueman-applet &
+# killall blueman-applet
+# blueman-applet &
+# killall blueberry-tray
+# blueberry-tray &
+# if [ -z $(pidof blueberry-tray) ]; then
+# fi
+# nm-applet &
+autossh -M 20000 -fNR 7004:localhost:2222 wyz@$DEBIAN_IP
