f9a3366e866fc98d3c880814ce689ed9b9f9db29
arch14 <wyz2016zxc@outlook.com>
2025-08-17 22:16:54 +0800

    backup at 25/08/17-22:16:54

--- tools/.config/todoman/config.py
+++ tools/.config/todoman/config.py
@@ -1,6 +1,6 @@
 # A glob expression which matches all directories relevant.
-path = "~/.local/share/calendar_radicale/*"
+path = "~/.local/share/radicale/collections/collection-root/hiraeth/*"
 date_format = "%Y-%m-%d"
 time_format = "%H:%M"
-default_list = "math"
+default_list = "todo"
 default_due = 0
