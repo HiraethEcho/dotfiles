9758f3db1867805eb13c6117bb4273dad72b2d5e
arch14 <wyz2016zxc@outlook.com>
2026-06-11 20:45:36 +0800

    update

--- config/.config/aconfmgr/99-unsorted.sh
+++ config/.config/aconfmgr/99-unsorted.sh
@@ -1,6 +1,6 @@
 
 
-# Sun Feb  8 12:12:10 PM CST 2026 - New / changed files
+# Wed Jun 10 09:24:29 PM CST 2026 - New / changed files
 
 
 CreateFile /etc/.pwd.lock 600 > /dev/null
@@ -9,7 +9,7 @@ CopyFile /etc/artalk/artalk.yml '' artalk artalk
 CreateDir /etc/audisp
 CreateDir /etc/audit/plugins.d 750
 CreateDir /etc/audit/rules.d
-CopyFile /etc/cloudflared/config.yml
+CreateDir /etc/cloudflared
 CreateDir /etc/colord '' colord colord
 CopyFile /etc/default/grub
 CopyFile /etc/docker/daemon.json
@@ -23,6 +23,7 @@ CreateLink /etc/libvirt/qemu/networks/autostart/default.xml /etc/libvirt/qemu/ne
 CopyFile /etc/libvirt/qemu/win10iot.xml 600
 CopyFile /etc/locale.conf
 CopyFile /etc/locale.gen
+CopyFile /etc/locale.gen.pacnew
 CreateLink /etc/localtime /usr/share/zoneinfo/Asia/Shanghai
 CopyFile /etc/ly/save.txt
 CopyFile /etc/makepkg.conf
@@ -38,6 +39,9 @@ CopyFile /etc/modules-load.d/iptables.conf
 CopyFile /etc/npmrc 666
 CreateDir /etc/opensnitchd/rules
 CreateLink /etc/os-release ../usr/lib/os-release
+CopyFile /etc/paclist/lists/all.list-arch-old
+CopyFile /etc/paclist/lists/aur.list-arch-old
+CopyFile /etc/paclist/lists/official.list-arch-old
 CopyFile /etc/pacman.conf
 CopyFile /etc/pacman.d/mirrorlist
 CopyFile /etc/pacman.d/mirrorlist.pacnew
@@ -53,7 +57,9 @@ CopyFile /etc/systemd/system.conf
 CopyFile /etc/systemd/system.conf.pacnew
 CopyFile /etc/systemd/system/archhome-tunnel-5G.service
 CopyFile /etc/systemd/system/archhome-tunnel.service
+CreateLink /etc/systemd/system/autovt@.service /usr/lib/systemd/system/getty@.service
 CreateLink /etc/systemd/system/bluetooth.target.wants/bluetooth.service /usr/lib/systemd/system/bluetooth.service
+CopyFile /etc/systemd/system/clash-verge-service.service
 CopyFile /etc/systemd/system/cloudflared-update.service
 CopyFile /etc/systemd/system/cloudflared-update.timer
 CopyFile /etc/systemd/system/cloudflared.service
@@ -67,13 +73,18 @@ CopyFile /etc/systemd/system/getty@tty1.service.d/skip-username.conf
 CopyFile /etc/systemd/system/glance-custom.service
 CopyFile /etc/systemd/system/grub-btrfsd.service.d/override.conf
 CreateLink /etc/systemd/system/multi-user.target.wants/ali-tunnel.service /etc/systemd/system/ali-tunnel.service
+CreateLink /etc/systemd/system/multi-user.target.wants/apm-daily-update.service /usr/lib/systemd/system/apm-daily-update.service
 CreateLink /etc/systemd/system/multi-user.target.wants/avahi-daemon.service /usr/lib/systemd/system/avahi-daemon.service
+CreateLink /etc/systemd/system/multi-user.target.wants/clash-verge-service.service /etc/systemd/system/clash-verge-service.service
+CreateLink /etc/systemd/system/multi-user.target.wants/cloudflared.service /etc/systemd/system/cloudflared.service
 CreateLink /etc/systemd/system/multi-user.target.wants/cups.path /usr/lib/systemd/system/cups.path
 CreateLink /etc/systemd/system/multi-user.target.wants/cups.service /usr/lib/systemd/system/cups.service
 CreateLink /etc/systemd/system/multi-user.target.wants/ip6tables.service /usr/lib/systemd/system/ip6tables.service
 CreateLink /etc/systemd/system/multi-user.target.wants/iptables.service /usr/lib/systemd/system/iptables.service
 CreateLink /etc/systemd/system/multi-user.target.wants/iwd.service /usr/lib/systemd/system/iwd.service
 CreateLink /etc/systemd/system/multi-user.target.wants/keyd.service /usr/lib/systemd/system/keyd.service
+CreateLink /etc/systemd/system/multi-user.target.wants/memos.service /usr/lib/systemd/system/memos.service
+CreateLink /etc/systemd/system/multi-user.target.wants/radicale-hiraeth.service /etc/systemd/system/radicale-hiraeth.service
 CreateLink /etc/systemd/system/multi-user.target.wants/remote-fs.target /usr/lib/systemd/system/remote-fs.target
 CreateLink /etc/systemd/system/multi-user.target.wants/sshd.service /usr/lib/systemd/system/sshd.service
 CreateLink /etc/systemd/system/multi-user.target.wants/tlp.service /usr/lib/systemd/system/tlp.service
@@ -88,6 +99,7 @@ CreateLink /etc/systemd/system/sockets.target.wants/cups.socket /usr/lib/systemd
 CreateLink /etc/systemd/system/sockets.target.wants/systemd-userdbd.socket /usr/lib/systemd/system/systemd-userdbd.socket
 CopyFile /etc/systemd/system/star-tunnel.service
 CreateLink /etc/systemd/system/sysinit.target.wants/systemd-timesyncd.service /usr/lib/systemd/system/systemd-timesyncd.service
+CreateLink /etc/systemd/system/timers.target.wants/cloudflared-update.timer /etc/systemd/system/cloudflared-update.timer
 CreateLink /etc/systemd/system/timers.target.wants/fstrim.timer /usr/lib/systemd/system/fstrim.timer
 CreateLink /etc/systemd/system/timers.target.wants/snapper-boot.timer /usr/lib/systemd/system/snapper-boot.timer
 CreateLink /etc/systemd/system/timers.target.wants/snapper-cleanup.timer /usr/lib/systemd/system/snapper-cleanup.timer
@@ -102,49 +114,16 @@ CreateLink /etc/systemd/user/sockets.target.wants/pipewire.socket /usr/lib/syste
 CopyFile /etc/systemd/zram-generator.conf
 CopyFile /etc/udev/rules.d/99-uinput.rules
 CreateDir /etc/userdb
-CopyFile /etc/zsh/zprofile
 CopyFile /etc/zsh/zshenv
+CreateLink /host /
 
 
-# Sun Feb  8 12:12:11 PM CST 2026 - New file properties
+# Wed Jun 10 09:24:30 PM CST 2026 - New file properties
 
 
 SetFileProperty /etc/artalk group artalk
 SetFileProperty /etc/artalk mode 750
 SetFileProperty /etc/artalk owner artalk
-
-
-# Thu May 21 02:43:38 PM CST 2026 - Extra files
-
-
-RemoveFile /etc/zsh/zprofile
-RemoveFile /etc/cloudflared/config.yml
-
-
-# Thu May 21 02:43:38 PM CST 2026 - New / changed files
-
-
-CopyFile /etc/locale.gen.pacnew
-CopyFile /etc/paclist/lists/all.list-arch-old
-CopyFile /etc/paclist/lists/aur.list-arch-old
-CopyFile /etc/paclist/lists/official.list-arch-old
-CreateLink /etc/systemd/system/autovt@.service /usr/lib/systemd/system/getty@.service
-CreateLink /etc/systemd/system/multi-user.target.wants/apm-daily-update.service /usr/lib/systemd/system/apm-daily-update.service
-CreateLink /etc/systemd/system/multi-user.target.wants/cloudflared.service /etc/systemd/system/cloudflared.service
-CreateLink /etc/systemd/system/multi-user.target.wants/memos.service /usr/lib/systemd/system/memos.service
-CreateLink /etc/systemd/system/multi-user.target.wants/radicale-hiraeth.service /etc/systemd/system/radicale-hiraeth.service
-CreateLink /etc/systemd/system/timers.target.wants/cloudflared-update.timer /etc/systemd/system/cloudflared-update.timer
-CreateLink /host /
-CopyFile /etc/fstab
-CopyFile /etc/mkinitcpio.conf.pacnew
-CopyFile /etc/pacman.d/mirrorlist.pacnew
-CopyFile /etc/systemd/system/cloudflared.service
-CopyFile /etc/systemd/system/copilot-api.service
-
-
-# Thu May 21 02:43:38 PM CST 2026 - New file properties
-
-
 SetFileProperty /etc/radicale/users group radicale
 SetFileProperty /etc/radicale/users mode 640
 SetFileProperty /etc/radicale/users owner radicale
